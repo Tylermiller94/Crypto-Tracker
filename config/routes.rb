4131b8abb96f92f612047c1a22e8a051d1ff87b0
Guest User <Guest@epicodus-39.local>
2017-12-04 12:07:20 -0800

    setup pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+  get 'forum', to: 'posts#index'
+  get '/', to: 'pages#home'
+  get 'about', to: 'pages#about'
   devise_for :users
   get 'hello_world', to: 'hello_world#index'
-  root to: "posts#index"
+  root to: "pages#home"
 
   resources :posts
 end
